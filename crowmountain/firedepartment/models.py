735ad4741fe4e32dc320cab50471113d1cf33c25
escleonh <hescalante@atu.edu>
2020-03-19 08:15:52 -0500

    Now working: Saving data from volunteer / Update data from volunteer

--- crowmountain/firedepartment/models.py
+++ crowmountain/firedepartment/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Citizen(models.Model):
+    VolunteerID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    First_Name = models.CharField(max_length=50, null=True)
+    Last_Name =  models.CharField(max_length=50, null=True)
+    Phone = models.CharField(max_length=10,null=True)
+    Address = models.CharField(max_length=100, null=True)
+    Age = models.IntegerField(null=True)
+    Email = models.EmailField(max_length=30, null=True)
+class Volunteer(models.Model):
+    status =(
+        ("Pending", "Pending"),
+        ("Accepted", "Accepted"),
+        ("Rejected", "Rejected"),
+    )
+    Citizen = models.ForeignKey(Citizen, on_delete=models.CASCADE)
+    Acceptance_Status = models.CharField(max_length=50, null=True, choices=status)
+    Date_Form_Submitted = models.DateField(auto_now_add=True, null=True)
+     
--- crowmountain/firedepartment/views.py
+++ crowmountain/firedepartment/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Citizen
+from .models import Volunteer
+from django.shortcuts import redirect
+#Sendgrid imports
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+#####################################
 
 # Create your views here.
 def index(request):
@@ -11,7 +19,72 @@ def contactus(request):
     return render(request, 'pages/contactus.html')
 
 def volunteer(request):
+    print(request.POST.get('errormsg'))
+
+    if request.POST.get("came_from")=="updateform":
+        firstname = request.POST.get("firstName")
+        lastname = request.POST.get("lastName")
+        address = request.POST.get("volunteerAddress")
+        age = request.POST.get("age")
+        phone = request.POST.get("phoneNumber")
+        email = request.POST.get("email")
+        volunteerid =request.POST.get("volunteerid")
+        Citizen.objects.filter(VolunteerID=volunteerid).update(First_Name=firstname, Last_Name=lastname, Phone=phone, Address=address,Age=age, Email=email)
+        
+        
+
+    elif request.POST.get("came_from")=="volunteerform":
+        firstname = request.POST.get("firstName")
+        lastname = request.POST.get("lastName")
+        address = request.POST.get("volunteerAddress")
+        city = request.POST.get("city")
+        zipcode = request.POST.get("zipCode")
+        age = request.POST.get("age")
+        phone = request.POST.get("phoneNumber")
+        email = request.POST.get("email")
+        
+        fulladdress = address + " " + city + ", AR " + zipcode
+        citizendata = Citizen(First_Name=firstname, Last_Name=lastname, Phone=phone, Address=fulladdress,Age=age, Email=email)
+
+        citizendata.save()
+        citizen = citizendata
+
+        volunterdata = Volunteer(Citizen=citizen, Acceptance_Status="Accepted")
+        volunterdata.save()
+       
+        message = Mail(
+        from_email=email,
+        to_emails='hescalante@atu.edu',
+        subject='New Volunteer',
+        html_content='<strong>Hola</strong>')
+        try:
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            status = response.status_code
+            return render(request, 'pages/volunteer/volunteer.html', {'status':status, 'email':email})
+            
+        except Exception as e:
+            print(e.message)
+
+    elif request.POST.get("came_from")=="volunteerIDForm":
+        print("from same s")
+        citizens = Citizen.objects.all()
+        vid=request.POST.get("volunteerID")
+        try:
+            volunteer = citizens.get(VolunteerID=vid)
+        except Exception as e:
+             return render(request, 'pages/volunteer/volunteer.html', {"errormsg":"Invalid ID"})
+    
+        return render(request, 'pages/volunteer/update.html', {'volunteer':volunteer})
+
+
     return render(request, 'pages/volunteer/volunteer.html')
+
+def volunteerform(request):
+    return render(request, 'pages/volunteer/form.html')
+
+def updateform(request):
+    return render(request, 'pages/volunteer/update.html', {'volunteer':volunteer})
 
 def burnnotice(request):
     return render(request, 'pages/burnnotice/burnnotice.html')
